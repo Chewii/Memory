2f55dbd322534ebb75feac10fc12d74fd3b58926
Chewii <drasmus@student.chalmers.se>
2014-10-14 16:50:15 +0200

    Update Kort.java

--- Kort.java
+++ Kort.java
@@ -1,36 +1,46 @@
-package Game;
-
 import javax.swing.Icon;
 import javax.swing.JButton;
+import java.awt.*;
 
-public class Kort extends JButton{
-	
-	private Kort copy;
+public class Kort extends JColorfulButton{
+		
+	private Icon icon;
 	private Status status = Status.SAKNAS;
-	private Icon icon = super.getIcon();
 	
 	public Kort(Icon i){
-		
 		this.icon = i;
+		setStatus(status);
 		
 	}
 	
 	public Kort(Icon i, Status status){	
-
-		super.setIcon(i);
-		this.status = status;
+		this.icon = i;
+		setStatus(status);
 		
 	}
 	
-	public static enum Status{
+	public enum Status{
 		
 		DOLT, SYNLIGT, SAKNAS;
 		
 	}
 	
 	public void setStatus(Status status) {
-		
 		this.status = status;
+		if(status == Status.SAKNAS){
+			super.setIcon(null);
+			this.setBackground(Color.white);
+			
+		}else if(status == Status.DOLT){
+			super.setIcon(null);
+			this.setBackground(Color.blue);
+			
+		}else{
+			super.setIcon(icon);
+			this.setBackground(Color.blue);
+			
+		}
+		
 		
 	}
 	
@@ -41,19 +51,14 @@ public class Kort extends JButton{
 	}
 	
 	public Kort copy(){
-
-		return this.copy;
+		return new Kort(this.icon, this.status);
 		
 	}
 	
 	public boolean sammaBild(Kort kort){
 		
-		if(kort.icon != null){
-		
-			
-			
-		}
-		return false;
+			return this.getIcon() == kort.getIcon();
+
 	}
 	
 }
